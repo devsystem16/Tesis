9cd02cb27282d086d4be2bb2ac67602562fcd5e0
Mychael castro <desarrollos16@hotmail.com>
2017-11-08 10:01:40 -0500

    Finish primer problema

--- New Unity Project 1/Assets/ScriptsGame/EstacionamientoController.cs
+++ New Unity Project 1/Assets/ScriptsGame/EstacionamientoController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EstacionamientoController : MonoBehaviour {
 
@@ -10,12 +11,39 @@ public class EstacionamientoController : MonoBehaviour {
     public GameObject ilustracionExpresion2;
     public GameObject ilustracionExpresion3;
 
-
+    public GameObject Estadocorrecto;
+    public GameObject Estadoincorrecto;
 
 
 
     // Use this for initialization
-    void Start () {
+    private List<string>    listaNumeroBuses;
+    private int pivot;
+    void Awake() {
+        listaNumeroBuses = new List<string>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            string dato = (Mathf.Round(Random.Range(1f, 30f))).ToString();
+            if (!listaNumeroBuses.Contains(dato)) {
+                listaNumeroBuses.Add(dato);
+            }
+        }
+
+    } 
+    void Start()
+    {
+        
+      
+        GameObject numeroBus1 = GameObject.Find("numeroBus1"); numeroBus1.GetComponent<TextMesh>().text = listaNumeroBuses[0];
+        GameObject numeroBus2 = GameObject.Find("numeroBus2"); numeroBus2.GetComponent<TextMesh>().text = listaNumeroBuses[1];
+        GameObject numeroBus3 = GameObject.Find("numeroBus3"); numeroBus3.GetComponent<TextMesh>().text = listaNumeroBuses[2];
+
+ 
+        Text  textoProblema =  GameObject.Find("Text").GetComponent<Text>() ;
+          pivot = int.Parse(Mathf.Round(Random.Range(0f, 2f)).ToString());
+        textoProblema.text = "Seleccione el bus con placa  " +listaNumeroBuses[pivot] + "  a Jaime"
+;
 
     }
 	
@@ -54,15 +82,31 @@ public class EstacionamientoController : MonoBehaviour {
             exprecion.Add(ilustracionExpresion1);
             exprecion.Add(ilustracionExpresion2);
             exprecion.Add(ilustracionExpresion3);
- 
-             
-            if (Constantes.concatenar(exprecion).Equals("jaime,igul,bus40")) {
 
-                Debug.Log("Correcto");
+
+            string bs = "bus" + listaNumeroBuses[pivot];
+            if (Constantes.concatenar(exprecion).Equals("jaime,igual," + bs))
+            {
+
+                Estadocorrecto.SetActive(true);
+            }
+            else {
+                Estadoincorrecto.SetActive(true);
+                StartCoroutine(incorrecto());
             }
  
 
         }
+    }
+
+
+    IEnumerator incorrecto() {
+        yield return new WaitForSeconds(3f);
+
+        Estadoincorrecto.SetActive(false);
+        ilustracionExpresion1.SetActive(false);
+        ilustracionExpresion2.SetActive(false);
+        ilustracionExpresion3.SetActive(false);
     }
 
 }
