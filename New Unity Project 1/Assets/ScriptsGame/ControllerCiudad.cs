f22b4c502a3c49e46ee86b30f2f8c62c2b43cf56
mcastro <desarrollos16@hotmail.com>
2017-12-16 14:57:16 -0500

    version

---
+++ New Unity Project 1/Assets/ScriptsGame/ControllerCiudad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+public class ControllerCiudad : MonoBehaviour {
+
+
+	// Muestra de mensajes
+	public GameObject flowChart;
+	public GameObject sayDialog;
+	public Say mensaje;
+	public GameObject storyText;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
